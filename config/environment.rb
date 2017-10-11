32c0d1c987468b8d13cc7e2c047ff698c6d6d404
Camilo Rivas <camilo@modyo.com>
2017-10-11 12:48:42 -0300

    fix email credentials

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['EMAIL_USERNAME'],
-  :password => ENV['EMAIL_PASSWORD'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'restror.herokuapp.com',
-  :address => ENV['EMAIL_ADDRESS'],
-  :port => ENV['EMAIL_PORT'],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
